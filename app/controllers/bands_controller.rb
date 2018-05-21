522b89c911c35a5c497abbeacac6f29e5fc6d637
estellegresillon <estelle.gresillon@gmail.com>
2018-05-21 18:45:27 +0200

    controllers

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,2 +1,14 @@
 class BandsController < ApplicationController
+
+  def index
+    @bands = Band.all
+  end
+
+  def show
+    @band = Band.find(params[:id])
+    @booking = Booking.new
+  end
+
+
+
 end
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,14 @@
 class BookingsController < ApplicationController
+
+  def create
+    @booking = Booking.new(booking_params)
+    @band = Band.find(params[:band_id])
+    @band.booking = @booking
+    if @band.save
+      redirect_to bands_path
+    else
+      render bands_path
+    end
+  end
+
 end
