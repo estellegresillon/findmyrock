2759d259f37d2daa73b7668c0cba5a1df998fe7d
estellegresillon <estelle.gresillon@gmail.com>
2018-05-22 15:35:02 +0200

    fixed

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,6 +4,7 @@ class ProfilesController < ApplicationController
     @bookings = current_user.bookings
     if current_user.band.present?
       @peoplewhobookedme = current_user.band.bookings
+      @avatar_band = current_user.band.photo
     else
       @peoplewhobookedme = []
     end
