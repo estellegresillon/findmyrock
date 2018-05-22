268ce7191d060856d70f7baa2067d624b94776ad
Benjamin Debray <shouty91600@hotmail.fr>
2018-05-22 18:39:19 +0200

    Accept button and pending/validate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_22_114412) do
+ActiveRecord::Schema.define(version: 2018_05_22_151025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_05_22_114412) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "price"
+    t.string "status"
     t.index ["band_id"], name: "index_bookings_on_band_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
