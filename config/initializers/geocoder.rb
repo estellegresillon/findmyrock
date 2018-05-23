3470f5d7176ee2d6aedfda999193238ae6c9e819
estellegresillon <estelle.gresillon@gmail.com>
2018-05-23 16:30:51 +0200

    key

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   use_https: true,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: ENV['GOOGLE_API_SERVER_KEY'],
+  api_key: ENV['AIzaSyChKRiRnwf-VkSvj_4g15ILpKiFuby-_jM'],
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
