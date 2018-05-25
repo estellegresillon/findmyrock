65fbf90e506cb22e367615057d4f30a81111efe6
estellegresillon <estelle.gresillon@gmail.com>
2018-05-25 10:53:41 +0200

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,6 @@ Booking.destroy_all
 Band.destroy_all
 User.destroy_all
 
-Genre.create(name: "Rock")
-Genre.create(name: "Pop")
-Genre.create(name: "Metal")
-Genre.create(name: "Blues")
-
 first_user = User.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -31,27 +26,27 @@ first_band = Band.create(
   )
 
 
-50.times do
-  booking = Booking.create(
-    user: User.last,
-    address: "Paris",
-    date: Date.new(2018, 5, 14),
-    band: Band.last
-    )
-  new_user = User.create(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    password: Faker::Lorem.characters(8)
-    )
-  new_band = Band.create(
-    name: Faker::Superhero.name,
-    description: Faker::Lorem.sentence,
-    price_per_hour: Random.rand(100..15000),
-    number_of_musicians: Random.rand(1..6),
-    address: ["Paris", "Dijon", "Reims"].sample,
-    music_style: ["Rock", "Metal", "Pop", "Blues"].sample,
-    user: new_user
-    )
-end
+# 50.times do
+#   booking = Booking.create(
+#     user: User.last,
+#     address: "Paris",
+#     date: Date.new(2018, 5, 14),
+#     band: Band.last
+#     )
+#   new_user = User.create(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email: Faker::Internet.email,
+#     password: Faker::Lorem.characters(8)
+#     )
+#   new_band = Band.create(
+#     name: Faker::Superhero.name,
+#     description: Faker::Lorem.sentence,
+#     price_per_hour: Random.rand(100..15000),
+#     number_of_musicians: Random.rand(1..6),
+#     address: ["Paris", "Dijon", "Reims"].sample,
+#     music_style: ["Rock", "Metal", "Pop", "Blues"].sample,
+#     user: new_user
+#     )
+#end
 
